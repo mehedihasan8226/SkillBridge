2a1f0b8516c05d0fda656f98f442e1974c60115d
Mehedi Hasan <mehedihassangd@gmail.com>
2026-01-27 14:31:15 +0600

    added review model

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,24 @@
 import app from "./app";
+import { prisma } from "./lib/prisma";
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, ()=>{
-    console.log(`server runnin on ${PORT}`);
-    
-})
+async function server() {
+    try {
+        await prisma.$disconnect()
+
+        app.listen(PORT, () => {
+            console.log(`server runnin on ${PORT}`);
+
+        })
+
+    } catch (error) {
+        console.error(error)
+        await prisma.$disconnect()
+        process.exit(1)
+
+    }
+
+}
+
+server()
